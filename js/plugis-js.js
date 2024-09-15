5b0e2e70640ab4debda6a27618cf6e48b705ce7b
IcelangDog21 <2162820800@qq.com>
2024-09-15 10:03:58 +0800

    Update plugis-js.js

--- js/plugis-js.js
+++ js/plugis-js.js
@@ -1,3 +1,78 @@
+const { PAPI } = require('../../GMLIB-LegacyRemoteCallApi/lib/BEPlaceholderAPI-JS');
+
+// 生存服
+mc.listen("onServerStarted", () => {
+    const cmd = mc.newCommand("survival", "生存服|Survival Server", PermType.Any);
+    cmd.setAlias("c");
+    cmd.setCallback((_cmd, ori, _out, res) => {
+        ori.player.transServer("61.183.42.66", 50267)
+    });
+    cmd.overload([]);
+    cmd.setup();
+})
+
+// 假人
+mc.listen("onServerStarted", () => {
+    const DUMMY = mc.newCommand("dummy", "生成假人|spawn dummy", PermType.GameMasters);
+    DUMMY.setAlias("spl");
+    DUMMY.setEnum("add", ["add"]);
+    DUMMY.setEnum("remove", ["remove"]);
+    DUMMY.mandatory("str", ParamType.String);
+    DUMMY.optional("spmode", ParamType.Int, "spmode", 1);
+    DUMMY.mandatory("player", ParamType.Player);
+    DUMMY.mandatory("add", ParamType.Enum, "add", 1);
+    DUMMY.mandatory("remove", ParamType.Enum, "remove", 1);
+    DUMMY.overload(["add", "str", "spmode"]);
+    DUMMY.overload(["remove", "player"]);
+    DUMMY.setCallback((_cmd, ori, out, res) => {
+        let pla = ori.player;
+        let pos = pla.pos;
+
+        if (res.add == "add") {
+            let spl = mc.spawnSimulatedPlayer(res.str, pos)
+            if (res.spmode == 0) spl.setGameMode(0);
+            if (res.spmode == 1) spl.setGameMode(1);
+            if (res.spmode == 2) spl.setGameMode(2);
+        }
+        if (res.remove == "remove") {
+            try {
+                let sp = res.player[0];
+                if (!sp.isSimulatedPlayer()) return pla.tell("§4你应该选择一个假人");
+                sp.simulateDisconnect()
+            } catch (e) {
+                pla.tell("这个假人不存在");
+            }
+        }
+    });
+    DUMMY.setup();
+});
+
+// TPS
+mc.listen("onServerStarted", () => {
+    const TPS = mc.newCommand("tps", "TPS", PermType.GameMasters);
+    TPS.setCallback((_cmd, ori, out, res) => {
+        let server_tps = PAPI.translateString("%server_tps_colored_2%", ori.player);
+        if (!ori.player) { log(server_tps) };
+        if (ori.player) { ori.player.tell(`${server_tps}`) };
+    });
+    TPS.overload([]);
+    TPS.setup();
+});
+
+// ping
+mc.listen("onServerStarted", () => {
+    const PING = mc.newCommand("ping", "Ping", PermType.Any);
+    PING.setCallback((_cmd, ori, out, res) => {
+        let ping = ori.player.getDevice().avgPing
+        if (ping <= 100) { ping = "§a" + ping }
+        if (ping > 100 && ping <= 200) { ping = "§e" + ping }
+        if (ping > 200) { ping = "§c" + ping }
+        ori.player.tell(ping + "§rms")
+    });
+    PING.overload([]);
+    PING.setup();
+});
+
 var newcommands = {
     sdl: "是大佬",
     ssb: "是傻逼",
