479cbde164ac6077eb29af26f7aaa8b83ea45d98
IcelangDog21 <2162820800@qq.com>
2024-09-15 11:03:53 +0800

    Update plugins-cpp.cpp

--- cpp/plugins-cpp.cpp
+++ cpp/plugins-cpp.cpp
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <cstring>
 
+#define MAX 2
+
+int main() {
+    int i;
+    char text[100] = "";
+
+    for (i = 0; i < MAX; i++) {
+        strcat(text, MAX[i]);
+        printf("%s\n", text);
+    }
+
+    return 0;
+}
